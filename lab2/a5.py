8cfe00a5ddd86c0fe22c8bc17a5b1a9716b7961c
Chistobaev Robert <Chistobaev_RV_25@student.grsu.by>
2025-10-06 23:07:00 +0300

    Add files via upload

---
+++ lab2/a5.py
@@ -0,0 +1,7 @@
+a = int(input ("а: "))
+b = int(input ("b: "))
+
+if a % b == 0 :
+    print ('YES')
+else :
+    print ('NO')
---
+++ lab2/a7.py
@@ -0,0 +1,13 @@
+x1 = int(input("x1: "))
+y1 = int(input("y1: "))
+x2 = int(input("x2: "))
+y2 = int(input("y2: "))
+
+if (x1+y1) % 2 == 0 and (x2+y2) % 2 == 0:
+    print ('YES')
+    print ('WHITE')
+elif (x1+y1) % 2 != 0 and (x2+y2) % 2 != 0:
+    print ('YES')
+    print ('BLACK')
+else :
+    print ('NO')
