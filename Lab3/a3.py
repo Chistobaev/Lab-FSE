6570f6e4afc0a47414da48d2f24e70effff5ab98
Chistobaev Robert <Chistobaev_RV_25@student.grsu.by>
2025-10-28 10:31:15 +0300

    Исправил ошибку в a3 (elif)

--- Lab3/a3.py
+++ Lab3/a3.py
@@ -9,11 +9,11 @@ else :
     isp = (10000 - x1) + x2
 if isp <= 300:
     opl = 21
-if isp <= 600:
+elif isp <= 600:
     opl = 21 + 0.06 * (isp - 300)
-if isp <= 800:
+elif isp <= 800:
     opl = 21 + 0.06 * 300 + 0.04 * (isp - 600)
-if isp > 800:
+elif isp > 800:
     opl = 21 + 0.06 * 300 + 0.04 * 200 + 0.025 * (isp - 800)
 src = opl / isp
 print("Предыдущее: ", x1)
@@ -21,4 +21,5 @@ print("Текущее: ", x2)
 print("Использовано: ", isp)
 print("К оплате", round(opl, 2))
 print("Ср. цена м^3: ", round(src, 2))
+
 
